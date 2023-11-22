3eb0b8035ff8ca952ce199d2b301bed721ab0e38
alirezahoushmand76 <alireza.houshmand121@gmail.com>
2023-11-22 08:14:31 +0330

    First commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module bookig-app
+
+go 1.21.4
---
+++ main.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func main() {
+	var conferenceName string = "Go Conferenece"
+	const confrenceTicket int = 50
+	var remainingTicket uint = 50
+
+	remainingTicket = -100
+
+	fmt.Printf("conferenceTickets is %T remainingTickets is %T conferenceName %T\n", confrenceTicket, remainingTicket, conferenceName)
+
+	fmt.Printf("Welcome to %v booking application\n", conferenceName)
+	fmt.Printf("We have total of %v tickets and %v are still available\n", confrenceTicket, remainingTicket)
+	fmt.Println("Get your tickets here to attend")
+
+	var userName string
+	var userTickets int
+	//ask user for their name
+	userName = "Tom"
+	userTickets = 2
+	fmt.Printf("user %v booked %v tickets.\n", userName, userTickets)
+
+}
